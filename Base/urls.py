a11fb68543b4d5a7b17778591d10fa3297cbe57f
Nibedita-Mohapatro <nibeditamohapatro123@gmail.com>
2025-11-25 16:03:15 +0530

    Final commit

--- Base/urls.py
+++ Base/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),            # root page → home.html
-    path('about/', views.about, name='about'),    # /about/ → about.html
+    path('', views.home, name='home'),            
+    path('about/', views.about, name='about'),    
     path('projects/', views.projects, name='projects'),
-     path('skills/', views.skills, name='skills'),# /projects/ → projects.html
-    path('contact/', views.contact, name='contact'),     # POST target for form
+     path('skills/', views.skills, name='skills'),
+    path('contact/', views.contact, name='contact'),    
 ]
